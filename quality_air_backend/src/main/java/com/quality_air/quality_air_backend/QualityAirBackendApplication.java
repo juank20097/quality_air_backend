862eb4dac9d77f6b76a46d1916822d3674e73b13
martinezjairo-d1 <martinezjairo.d1@gmail.com>
2024-08-27 11:50:13 -0500

    carga inicial del proyecto

---
+++ quality_air_backend/src/main/java/com/quality_air/quality_air_backend/QualityAirBackendApplication.java
@@ -0,0 +1,13 @@
+package com.quality_air.quality_air_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityAirBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityAirBackendApplication.class, args);
+	}
+
+}
---
+++ quality_air_backend/src/test/java/com/quality_air/quality_air_backend/QualityAirBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quality_air.quality_air_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityAirBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
