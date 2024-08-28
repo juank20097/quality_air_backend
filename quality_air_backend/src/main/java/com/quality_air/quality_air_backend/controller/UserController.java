9d88b97571aa3cf034c145c693edf8295af429bb
martinezjairo-d1 <martinezjairo.d1@gmail.com>
2024-08-28 11:45:21 -0500

    Servicio de usuario

---
+++ quality_air_backend/src/main/java/com/quality_air/quality_air_backend/controller/UserController.java
@@ -0,0 +1,61 @@
+package com.quality_air.quality_air_backend.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.management.openmbean.OpenType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quality_air.quality_air_backend.entities.User;
+import com.quality_air.quality_air_backend.services.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	@Autowired
+	private UserService userservice;
+
+	//@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping
+	public List<User> getAll() {
+		return userservice.getAll();
+	}
+
+	//@CrossOrigin(origins = "http://localhost:4200")
+	@PostMapping
+	public User insert(@RequestBody User enter) {
+		return userservice.insert(enter);
+	}	
+
+	//@CrossOrigin(origins = "http://localhost:4200")
+	@PutMapping(path = { "/{id}" })
+	public User update(@RequestBody User enter, @PathVariable("id") int id) {
+		enter.setId(id);
+		return userservice.update(enter);
+	}
+
+	//@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping(path = { "/{id}" })
+	public Optional<User> getId(@PathVariable("id") int id) {
+		return userservice.getId(id);
+	}
+	
+	//@CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping("/login")
+    public String authenticate(@RequestParam("correo") String correo, @RequestParam("password") String password) {
+    	System.out.println("CORREO CONTROLER: "+correo+" PASSWORD CONTROLER"+password);
+    	return userservice.login(correo, password);
+    }
+
+}
---
+++ quality_air_backend/src/main/java/com/quality_air/quality_air_backend/repo/UserRepo.java
@@ -0,0 +1,21 @@
+	package com.quality_air.quality_air_backend.repo;
+	
+	import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+	import org.springframework.data.jpa.repository.Query;
+	import org.springframework.stereotype.Repository;
+	
+	import com.quality_air.quality_air_backend.entities.User;
+	
+	@Repository
+	public interface UserRepo extends JpaRepository<User, Integer> {
+		
+		@Query("select a from User a where a.status = true")
+		List<User> findByStatus();
+		
+		@Query("SELECT u FROM User u WHERE u.correo = :correo AND u.password = :password")
+	    Optional<User> findByCorreoAndPassword(String correo, String password);
+	
+	}
